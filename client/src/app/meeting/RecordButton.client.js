ab0da8d0548c3567bce985972d2440a5a1e66c18
Bill Zhang <billzhangsc@gmail.com>
2024-01-14 05:00:29 -0800

    Merge

--- client/src/app/meeting/RecordButton.client.js
+++ client/src/app/meeting/RecordButton.client.js
@@ -1,36 +1,55 @@
 'use client'
 
-import React, { useState } from 'react';
-import { Button } from '@mui/material';
-import { Mic } from '@mui/icons-material';
+import dynamic from 'next/dynamic'
+const VoiceActivityComponent = dynamic(() => import('./VoiceActivity'), {
+  ssr: false,
+})
+import io from 'socket.io-client'
+import React, {useState} from 'react'
+import {Button} from '@mui/material'
+import {Mic} from '@mui/icons-material'
 
+const socket = io('http://localhost:8000')
+
+const ROLE = 'therapist'
 
 export default function RecordButton() {
-  const [recording, setRecording] = useState(false);
+  const [recording, setRecording] = useState(false)
 
   function startRecording() {
-    setRecording(true);
+    setRecording(true)
   }
 
   function stopRecording() {
-    setRecording(false);
+    setRecording(false)
+  }
+
+  function onAudio(audio) {
+
+    socket.emit('audio_chunk', {
+      role: ROLE,
+      content: audio,
+    })
   }
 
   return (
-    <Button 
-      variant="contained" 
-      onClick={recording ? stopRecording : startRecording}
-      sx={{
-        borderRadius: '50%', 
-        width: '80px', 
-        height: '80px', 
-        backgroundColor: recording ? 'red' : 'blue',
-        '&:hover': {
-          backgroundColor: recording ? 'darkred' : 'darkblue',
-        },
-      }}
-    >
-      <Mic />
-    </Button>
-  );
+    <>
+      <Button
+        variant="contained"
+        onClick={recording ? stopRecording : startRecording}
+        sx={{
+          borderRadius: '50%',
+          width: '80px',
+          height: '80px',
+          backgroundColor: recording ? 'red' : 'blue',
+          '&:hover': {
+            backgroundColor: recording ? 'darkred' : 'darkblue',
+          },
+        }}
+      >
+        <Mic />
+        <VoiceActivityComponent recording={recording} onAudio={onAudio} />
+      </Button>
+    </>
+  )
 }
--- client/src/app/meeting/page.js
+++ client/src/app/meeting/page.js
@@ -1,6 +1,6 @@
-"use client";
+'use client'
 import backgroundImg from '../../../src/image/backgroundContain.png'
-import React from "react";
+import React from 'react'
 import {
   Drawer,
   List,
@@ -15,74 +15,76 @@ import {
   LinearProgress,
   Card,
   CardContent,
-} from "@mui/material";
-import HomeIcon from "@mui/icons-material/Home";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import SettingsIcon from "@mui/icons-material/Settings";
-import Tag from "@/components/Tag";
+} from '@mui/material'
+import HomeIcon from '@mui/icons-material/Home'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'
+import SettingsIcon from '@mui/icons-material/Settings'
+import Tag from '@/components/Tag'
 // import {theme} from '../theme';
-import { PlayArrow as PlayArrowIcon } from "@mui/icons-material";
-import RecordButton from "./RecordButton.client";
-import "../../app/global.css";
-import AccessTimeFilledIcon from "@mui/icons-material/AccessTimeFilled";
-import ArrowBackRoundedIcon from "@mui/icons-material/ArrowBackRounded";
-import MicIcon from "@mui/icons-material/Mic";
-import ChatIcon from "@mui/icons-material/Chat";
+import {PlayArrow as PlayArrowIcon} from '@mui/icons-material'
+import RecordButton from './RecordButton.client'
+import '../../app/global.css'
+import AccessTimeFilledIcon from '@mui/icons-material/AccessTimeFilled'
+import ArrowBackRoundedIcon from '@mui/icons-material/ArrowBackRounded'
+import MicIcon from '@mui/icons-material/Mic'
+import ChatIcon from '@mui/icons-material/Chat'
 
 function MainContent() {
   return (
     <Box
       sx={{
-        width: "100vw",
+        width: '100vw',
         flexGrow: 1,
         p: 3,
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "left",
-        borderRadius: "8px",
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'left',
+        borderRadius: '8px',
       }}
     >
       <Typography
         variant="h4"
         sx={{
-          fontWeight: "800",
-          fontFamily: "Inter,sans-serif",
-          marginTop: "0px",
-          marginBottom: "8px"
+          fontWeight: '800',
+          fontFamily: 'Inter,sans-serif',
+          marginTop: '0px',
+          marginBottom: '8px',
         }}
       >
         Kael's Hackathon Therapy Session
       </Typography>
       <Stack direction="row" spacing={2}>
-          <Tag
-            borderColor="3px solid #FECEFF"
-            textColor="#D196D2"
-            tagName="adhd"
-            backgroundColor="#FFE7FF"
-          />
-          <Tag
-            borderColor="3px solid #B7D8FF"
-            textColor="#66A1E7"
-            tagName="anxiety"
-            backgroundColor="#DCECFF"
-          />
-        </Stack>
-      <Divider sx={{
-     width: '100%', // Set the width
-     height: '4px', // Set the height (thickness) to 8px
-     my: '22px', // Set the vertical margin (marginTop and marginBottom) to 22px
-     mx: 0, // Ensure there are no horizontal margins
-     // To make the Divider appear as a line with 8px height, we use the border style
-     border: 0,
-     backgroundColor: 'rgba(0, 0, 0, 0.05);',
-  }} />
+        <Tag
+          borderColor="3px solid #FECEFF"
+          textColor="#D196D2"
+          tagName="adhd"
+          backgroundColor="#FFE7FF"
+        />
+        <Tag
+          borderColor="3px solid #B7D8FF"
+          textColor="#66A1E7"
+          tagName="anxiety"
+          backgroundColor="#DCECFF"
+        />
+      </Stack>
+      <Divider
+        sx={{
+          width: '100%', // Set the width
+          height: '4px', // Set the height (thickness) to 8px
+          my: '22px', // Set the vertical margin (marginTop and marginBottom) to 22px
+          mx: 0, // Ensure there are no horizontal margins
+          // To make the Divider appear as a line with 8px height, we use the border style
+          border: 0,
+          backgroundColor: 'rgba(0, 0, 0, 0.05);',
+        }}
+      />
       <Typography
-          variant="h6"
-          sx={{
-            fontWeight: "800",
-            fontFamily: "Inter,sans-serif",
-          }}
-        >
+        variant="h6"
+        sx={{
+          fontWeight: '800',
+          fontFamily: 'Inter,sans-serif',
+        }}
+      >
         Record a live transcript
       </Typography>
       <Typography variant="body1" gutterBottom>
@@ -90,30 +92,30 @@ function MainContent() {
       </Typography>
       <Box
         sx={{
-          width: "80%",
+          width: '80%',
           height: 200,
-          borderRadius: "8px",
+          borderRadius: '8px',
           my: 2,
-          bgcolor: "grey.300",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
+          bgcolor: 'grey.300',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
         }}
       >
         <RecordButton />
       </Box>
       <Box
         sx={{
-          width: "80%",
+          width: '80%',
           height: 200,
-          borderRadius: "8px",
+          borderRadius: '8px',
           my: 2,
-          bgcolor: "grey.300",
-          overflowY: "auto", // add this line
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "left",
-          justifyContent: "flex-start",
+          bgcolor: 'grey.300',
+          overflowY: 'auto', // add this line
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'left',
+          justifyContent: 'flex-start',
         }}
       >
         <Typography variant="h5" margin="10px" color="gray" gutterBottom>
@@ -133,7 +135,7 @@ function MainContent() {
       </Box>
     </Box>
     // </ThemeProvider>
-  );
+  )
 }
 
 /* function StatsSection() {
@@ -157,165 +159,171 @@ function MainContent() {
   );
 } */
 
-function SuggestionBox({ tip }) {
+function SuggestionBox({tip}) {
   return (
-    <Card
-      sx={{ minWidth: 275, marginBottom: "10px", border: "1px solid gray" }}
-    >
+    <Card sx={{minWidth: 275, marginBottom: '10px', border: '1px solid gray'}}>
       <CardContent>
         <Typography variant="body1">{tip}</Typography>
       </CardContent>
     </Card>
-  );
+  )
 }
 
 function RightPanel() {
   return (
     <Box
       sx={{
-        width: "50%",
-        height: "calc(100vh - 32px)",
+        width: '50%',
+        height: 'calc(100vh - 32px)',
         padding: '22px',
-        borderRadius: "10px",
-        backgroundColor: "rgba(255, 255, 255, 0.50)",
+        borderRadius: '10px',
+        backgroundColor: 'rgba(255, 255, 255, 0.50)',
       }}
     >
-      <Box
-        
-      >
-       {/* patient mood */}
-        <Box sx={{ display: "flex", alignItems: "center", width:"100%",
-      gap: "12px",
-   
-      justifyContent :"center",
-      alignItems :"center",
+      <Box>
+        {/* patient mood */}
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            width: '100%',
+            gap: '12px',
 
-    
-      backgroundColor : "#fff",
-          borderRadius : "20px",
-          padding : "18px",
-  
-          width : "300px",
-          alignContent : "center" }}>
+            justifyContent: 'center',
+            alignItems: 'center',
+
+            backgroundColor: '#fff',
+            borderRadius: '20px',
+            padding: '18px',
+
+            width: '300px',
+            alignContent: 'center',
+          }}
+        >
           <Box
             sx={{
               width: 40,
               height: 40,
-              bgcolor: "white",
-              borderRadius: "8px",
-              display: "flex",
-              gap: "12px",
-              justifyContent: "center",
-              alignItems: "center",
-              marginRight: "10px",
+              bgcolor: 'white',
+              borderRadius: '8px',
+              display: 'flex',
+              gap: '12px',
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginRight: '10px',
             }}
           >
-           <span
-  role="img"
-  aria-label="happy"
-  style={{
-    fontSize: '30px', // Adjust the size as needed
-    backgroundColor: '#E2E2E2',
-    borderRadius: '22px',
-    gap: "12px",
-  
-    margin: '0px',
-    width: 'fit-content',
-    padding: '10px', // Add some padding around the emoji
-    display: 'inline-flex', // Use inline-flex to center the emoji and enable padding
-    justifyContent: 'center',
-    alignItems: 'center',
-    
-  }}
->
-  😊
-</span>
+            <span
+              role="img"
+              aria-label="happy"
+              style={{
+                fontSize: '30px', // Adjust the size as needed
+                backgroundColor: '#E2E2E2',
+                borderRadius: '22px',
+                gap: '12px',
+
+                margin: '0px',
+                width: 'fit-content',
+                padding: '10px', // Add some padding around the emoji
+                display: 'inline-flex', // Use inline-flex to center the emoji and enable padding
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              😊
+            </span>
           </Box>
           <Stack width="100%" direction="column">
-            <Typography variant="p"
-          gutterBottom
-          color="text.primary"
-          sx={{
-            fontWeight: "500",
-            fontFamily: "Inter,sans-serif",
-            color: "rgba(0,0,0,.5)",
-            fontSize: "16px",
-            
-          }}>
-          Patient Mood
-        </Typography>
-          <Typography variant="h6"
-          sx={{
-            fontWeight: "800",
-            fontFamily: "Inter,sans-serif",
-            fontSize: '24px'
-          }}>Happy</Typography>
+            <Typography
+              variant="p"
+              gutterBottom
+              color="text.primary"
+              sx={{
+                fontWeight: '500',
+                fontFamily: 'Inter,sans-serif',
+                color: 'rgba(0,0,0,.5)',
+                fontSize: '16px',
+              }}
+            >
+              Patient Mood
+            </Typography>
+            <Typography
+              variant="h6"
+              sx={{
+                fontWeight: '800',
+                fontFamily: 'Inter,sans-serif',
+                fontSize: '24px',
+              }}
+            >
+              Happy
+            </Typography>
           </Stack>
-          
         </Box>
       </Box>
       <Box
-         width="100%"
-         height="100%"
-         direction="column"
-         justifyContent="center"
-         alignItems="center"
-         spacing={2}
-       
-         backgroundColor= "#fff"
-             borderRadius= "40px"
-             padding= "32px 22px"
-            border= "4px solid rgba(0,0,0, .1)"
-             width= "300px"
-             alignContent= "center"
+        width="100%"
+        height="100%"
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        spacing={2}
+        backgroundColor="#fff"
+        borderRadius="40px"
+        padding="32px 22px"
+        border="4px solid rgba(0,0,0, .1)"
+        width="300px"
+        alignContent="center"
       >
-        <Typography variant="h4" gutterBottom
-      sx={{
-        fontWeight:'800',
-        fontFamily:'Inter, sans-serif',
-        fontSize:'24px'
-      }}>
+        <Typography
+          variant="h4"
+          gutterBottom
+          sx={{
+            fontWeight: '800',
+            fontFamily: 'Inter, sans-serif',
+            fontSize: '24px',
+          }}
+        >
           Recommendations
         </Typography>
-        <Box sx={{
-    border: "4px solid rgba(0, 0, 0, .1)",
-    borderRadius: "20px",
-    padding: "22px",
-    fontWeight: "600",
-            fontFamily: "Inter,sans-serif",
-            fontSize: '16px'
-
-  }}>
-
-Stay calm and focused during the session.
-  </Box>
-  <Box sx={{
-    border: "4px solid rgba(0, 0, 0, .1)",
-    borderRadius: "20px",
-    padding: "22px",
-    fontWeight: "600",
-            fontFamily: "Inter,sans-serif",
-            fontSize: '16px'
-
-  }}>
-
-Stay calm and focused during the session.
-  </Box>
-  <Box sx={{
-    border: "4px solid rgba(0, 0, 0, .1)",
-    borderRadius: "20px",
-    padding: "22px",
-    fontWeight: "600",
-            fontFamily: "Inter,sans-serif",
-            fontSize: '16px'
-
-  }}>
-
-Stay calm and focused during the session.
-  </Box>
+        <Box
+          sx={{
+            border: '4px solid rgba(0, 0, 0, .1)',
+            borderRadius: '20px',
+            padding: '22px',
+            fontWeight: '600',
+            fontFamily: 'Inter,sans-serif',
+            fontSize: '16px',
+          }}
+        >
+          Stay calm and focused during the session.
+        </Box>
+        <Box
+          sx={{
+            border: '4px solid rgba(0, 0, 0, .1)',
+            borderRadius: '20px',
+            padding: '22px',
+            fontWeight: '600',
+            fontFamily: 'Inter,sans-serif',
+            fontSize: '16px',
+          }}
+        >
+          Stay calm and focused during the session.
+        </Box>
+        <Box
+          sx={{
+            border: '4px solid rgba(0, 0, 0, .1)',
+            borderRadius: '20px',
+            padding: '22px',
+            fontWeight: '600',
+            fontFamily: 'Inter,sans-serif',
+            fontSize: '16px',
+          }}
+        >
+          Stay calm and focused during the session.
+        </Box>
       </Box>
     </Box>
-  );
+  )
 }
 
 function Dashboard() {
@@ -338,13 +346,12 @@ function Dashboard() {
 
         height: '100vh', // Full height of the viewport
         width: '100vw',
-
-      }}// replace 'darkerColor' with the color you want
+      }} // replace 'darkerColor' with the color you want
     >
       <MainContent />
       <RightPanel />
     </Stack>
-  );
+  )
 }
 
-export default Dashboard;
+export default Dashboard
